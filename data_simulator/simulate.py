5f2a700df082500e7c0689b99ce0d98442e4c336
janmro1 <janmro9201@gmail.com>
2025-04-28 14:48:54 -0400

    Update simulate.py

--- data_simulator/simulate.py
+++ data_simulator/simulate.py
@@ -1,29 +1,36 @@
-import json
-import notecard
-import notecard_pseudo_sensor
-from periphery import I2C
+import csv
+import random
 import time
+import paho.mqtt.client as mqtt
 
-productUID = "com. .your-name:your_product"
+MQTT_BROKER = "mqtt.example.com"  # address of MQTT servor/broker
+MQTT_TOPIC = "sensor/data" # topic being published
 
-port = I2C("/dev/i2c-1")
-card = notecard.OpenI2C(port, 0, 0)
+client = mqtt.Client()
+client.connect(MQTT_BROKER)
 
-req = {"req": "hub.set"}
-req["product"] = productUID
-req["mode"] = "continuous"
- 
-print(json.dumps(req))
- 
-rsp = card.Transaction(req)
-print(rsp)
+filename = "output.csv"
 
-sensor = notecard_pseudo_sensor.NotecardPseudoSensor(card)
+def generate_data():
+    timestamp = time.time()
+    temp = random.randint(70, 90)
+    vibration = random.randint(0, 20)
+    failure = 1 if temp > 80 and vibration > 10 else 0
+
+    return [timestamp, temp, vibration, failure]
+
+def write_to_csv(data):
+    with open(filename, mode="w", newline="") as file:
+        writer = csv.writer(file) # creates an object that can write row(s) to a file
+        writer.writerow(data) # writes a single row
+
+def publish_mqtt(data):
+    payload = {"timestamp": data[0], "temperature": data[1], "vibration": data[2], "failure": data[3]}
+    client.publish(MQTT_TOPIC, str(payload))
 
 while True:
-  temp = sensor.temp()
-  humidity = sensor.humidity()
-  print('Temperature: {} degrees C'.format(temp))
-  print('Humidity: {}%'.format(humidity))
- 
-  time.sleep(15)
+    data_row = generate_data()
+    write_to_csv(data_row)
+    publish_mqtt(data_row)
+    print(f"Generated and published: {data_row}")
+    time.sleep(5)
