4f0ed4eba44151b3f5bd6dc38a1702bd810eb62e
janmro1 <janmro9201@gmail.com>
2025-04-28 17:17:35 -0400

    Update simulate.py

--- data_simulator/simulate.py
+++ data_simulator/simulate.py
@@ -6,17 +6,21 @@ import paho.mqtt.client as mqtt
 MQTT_BROKER = "localhost"  # address of MQTT servor/broker
 MQTT_TOPIC = "sensor/data" # topic being published
 
-client = mqtt.Client()
+client = mqtt.Client(callback_api_version=5)
 client.connect(MQTT_BROKER)
 
 filename = "output.csv"
+headers = ["timestamp", "temperature", "vibration", "failure_risk"]
+
+with open(filename, mode="a", newline="") as file:
+    writer = csv.writer(file) # creates an object that can write row(s) to a file
+    writer.writerow(headers) # writes a single row
 
 def generate_data():
     timestamp = time.time()
     temp = random.randint(70, 90)
     vibration = random.randint(0, 20)
     failure = 1 if temp > 80 and vibration > 10 else 0
-
     return [timestamp, temp, vibration, failure]
 
 def write_to_csv(data):
@@ -26,11 +30,12 @@ def write_to_csv(data):
 
 def publish_mqtt(data):
     payload = {"timestamp": data[0], "temperature": data[1], "vibration": data[2], "failure": data[3]}
+    print(payload)
     client.publish(MQTT_TOPIC, str(payload))
 
 while True:
     data_row = generate_data()
     write_to_csv(data_row)
     publish_mqtt(data_row)
-    print(f"Generated and published: {data_row}")
+    #print(f"Generated and published: {data_row}")
     time.sleep(5)
