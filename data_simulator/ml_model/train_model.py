530d1c68932f294caa1fa16374c75498792d7546
janmro1 <janmro9201@gmail.com>
2025-04-29 09:47:05 -0400

    Update train_model.py

--- data_simulator/ml_model/train_model.py
+++ data_simulator/ml_model/train_model.py
@@ -10,11 +10,12 @@ X = data[['temperature', 'vibration']]
 y = data['failure_risk']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-model = LogisticRegression()
+model = LogisticRegression(C=0.1, random_state=42)
+
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy: {accuracy: }")
+print(f"Accuracy: {accuracy}")
 
 joblib.dump(model, 'model.pkl') # saves model as model.pkl
