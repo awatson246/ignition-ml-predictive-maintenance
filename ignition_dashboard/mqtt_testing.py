253b52281b331d5072cd8e14bdd24ef1aa582b5d
Josh Miller <troymiller866@gmail.com>
2025-05-02 11:43:33 -0400

    simplified test

--- ignition_dashboard/mqtt_testing.py
+++ ignition_dashboard/mqtt_testing.py
@@ -1,15 +1,11 @@
 import paho.mqtt.client as mqtt
-import time
-import random
 
-broker = "192.168.106.9"  # use your Ignition Gateway IP if running MQTT Distributor there
-topic = "pi/temperature"
+broker = "192.168.103.13"  # Replace with your Ignition Gateway IP
+port = 1883
+topic = "my/test/topic"
+message = "Hello from VS Code!"
 
 client = mqtt.Client()
-client.connect(broker, 1883, 60)
-
-while True:
-    temp = random.randint(48, 52)
-    message = f"{temp}"
-    client.publish(topic, message)
-    time.sleep(1)
+client.connect(broker, port)
+client.publish(topic, message)
+client.disconnect()
